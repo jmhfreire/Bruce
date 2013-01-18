abf2dd25625d16fb34ed35c6640e60d781b18676
jmhfreire <jmhfreire@gmail.com>
2013-01-18 17:41:44 -0500

    Update ChallengeAccepted/src/PrintNum.java
    Testing the edit 

--- ChallengeAccepted/src/PrintNum.java
+++ ChallengeAccepted/src/PrintNum.java
@@ -1,14 +1,12 @@
-
 public class PrintNum {
-// took 2 seconds lolol
 	public static void main(String args[]){
 		for(int i=1;i<101;i++){
 			if(i%3 == 0 && i%5 == 0){
-				System.out.println("FizzBuzz");
+				System.out.println("TestingGit");
 			}else if(i%5 == 0){
-				System.out.println("Buzz");
+				System.out.println("Testing");
 			}else if(i%3 == 0){
-				System.out.println("Fizz");
+				System.out.println("Git");
 			}else{
 				System.out.println(i);
 			}
